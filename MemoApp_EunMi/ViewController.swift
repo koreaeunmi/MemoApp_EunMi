198258aef23cea9de14db140a6434c3035c56e75
koreaeunmi <koreaeunmi@yahoo.com>
2024-08-27 16:12:59 -0400

    Initial Commit

---
+++ MemoApp_EunMi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoApp_EunMi
+//
+//  Created by Jong Cheol Jeong on 8/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

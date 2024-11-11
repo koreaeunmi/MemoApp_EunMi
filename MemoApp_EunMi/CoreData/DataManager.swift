c5963b527d0e0914323ac9b43da3382acd5b0339
koreaeunmi <koreaeunmi@yahoo.com>
2024-11-11 09:46:45 -0500

    Before Edit

---
+++ MemoApp_EunMi/CoreData/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  MemoApp_EunMi
+//
+//  Created by Jong Cheol Jeong on 11/7/24.
+//
+
+import Foundation
---
+++ MemoApp_EunMi/noTextAlert.swift
@@ -0,0 +1,8 @@
+//
+//  noTextAlert.swift
+//  MemoApp_EunMi
+//
+//  Created by Jong Cheol Jeong on 9/24/24.
+//
+
+import Foundation
